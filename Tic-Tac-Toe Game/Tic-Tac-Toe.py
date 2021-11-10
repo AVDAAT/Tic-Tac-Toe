25cf8fd0b1e4ba67726627d8064bf48610e1633c
AVDAAT <74604609+AVDAAT@users.noreply.github.com>
2021-11-10 18:50:14 +0530

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe Game/Tic-Tac-Toe.py
+++ Tic-Tac-Toe Game/Tic-Tac-Toe.py
@@ -96,32 +96,30 @@ class TicTacToe:
 
             self.show_board()
 
-            # taking user input
+         
             row, col = list(
                 map(int, input("Enter row and column numbers to fix spot: ").split()))
             print()
 
-            # fixing the spot
+          
             self.fix_spot(row - 1, col - 1, player)
 
-            # checking whether current player is won or not
+          
             if self.is_player_win(player):
                 print(f"Player {player} wins the game!")
                 break
 
-            # checking whether the game is draw or not
             if self.is_board_filled():
                 print("Match Draw!")
                 break
 
-            # swapping the turn
+           
             player = self.swap_player_turn(player)
 
-        # showing the final view of board
+       
         print()
         self.show_board()
 
 
-# starting the game
 tic_tac_toe = TicTacToe()
 tic_tac_toe.start()
